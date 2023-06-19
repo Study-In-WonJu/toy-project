785c1db947c9b2156eaca1aba6b6ad1890944269
Khdscor <khds_cor@naver.com>
2023-06-19 21:27:17 +0900

    feat: error 처리 추가

---
+++ backend/toy/src/main/java/toy/toy/global/error/ErrorResponse.java
@@ -0,0 +1,13 @@
+package toy.toy.global.error;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private String errorMessage;
+
+    public ErrorResponse(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+}
