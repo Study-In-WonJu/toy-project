39826ab900e675b516ac4588253bba554feabd0d
Khdscor <khds_cor@naver.com>
2023-06-24 17:54:21 +0900

    feat: editContent controller 구현

--- backend/toy/src/main/java/toy/toy/domain/article/api/ChangeArticleController.java
+++ backend/toy/src/main/java/toy/toy/domain/article/api/ChangeArticleController.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import toy.toy.domain.article.application.ChangeArticleService;
+import toy.toy.domain.article.dto.EditArticleRequest;
 
 @RestController
 @RequestMapping("/article")
@@ -16,8 +19,15 @@ public class ChangeArticleController {
     private final ChangeArticleService changeArticleService;
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteArticle(@PathVariable Long id){
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
         changeArticleService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> editContent(@PathVariable Long id,
+        @RequestBody EditArticleRequest request) {
+        changeArticleService.edit(id, request.getContent());
         return ResponseEntity.noContent().build();
     }
 }
