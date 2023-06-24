229b12ff8a2340ed5df47dac2fb7312c830121b5
Khdscor <khds_cor@naver.com>
2023-06-24 16:13:47 +0900

    feat: findArticleDetails controller 구현

--- backend/toy/src/main/java/toy/toy/domain/article/api/FindArticleController.java
+++ backend/toy/src/main/java/toy/toy/domain/article/api/FindArticleController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import toy.toy.domain.article.application.FindArticleService;
+import toy.toy.domain.article.dto.ArticleDetailsResponse;
 import toy.toy.domain.article.dto.ArticleResponse;
 
 @RestController
@@ -20,5 +22,11 @@ public class FindArticleController {
     public ResponseEntity<List<ArticleResponse>> findArticles() {
         List<ArticleResponse> responses = findArticleService.findArticles();
         return ResponseEntity.ok().body(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDetailsResponse> findArticleOne(@PathVariable Long id) {
+        ArticleDetailsResponse response = findArticleService.findDetails(id);
+        return ResponseEntity.ok().body(response);
     }
 }
